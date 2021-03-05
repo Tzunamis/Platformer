844e75b726e887385e4029a2b43b4553f48c1b33
noahmacdonald13 <noahmacdonald13@gmail.com>
2021-03-05 16:33:24 -0500

    Added an incredibly ugly "Main Menu", someone please save me in my complete lack of artistic ability.

---
+++ Platformer/Assets/Scripts/MenuUI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUI : MonoBehaviour
+{
+    public GameObject Menu;
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void MinimizeMenu()
+    {
+        Time.timeScale = 1;
+        Menu.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
